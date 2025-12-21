7ef22ceb22d3bfbe3c3bf7c281d7755a1ececae9
acc1iela <acc1iela@gmail.com>
2025-12-21 15:28:03 +0900

    update: write types in typed.ts

--- src/types.ts
+++ src/types.ts
@@ -1,5 +1,43 @@
-export type Validators<T> = {
-  [K in keyof T]?: (value: T[K], values: T) => string | null;
+export type ValidatorResult = string | null;
+
+export type Validators<TValues extends Record<string, unknown>> = {
+  [K in keyof TValues]?: (value: TValues[K], values: TValues) => ValidatorResult;
 };
 
-export type Errors<T> = Partial<Record<keyof T, string>>;
+export type Errors<TValues extends Record<string, unknown>> = Partial<
+  Record<keyof TValues, string>
+>;
+
+export type Touched<TValues extends Record<string, unknown>> = Partial<
+  Record<keyof TValues, boolean>
+>;
+
+export type UseFormOptions<TValues extends Record<string, unknown>> = {
+  defaultValues: TValues;
+  validators?: Validators<TValues>;
+};
+
+export type RegisterReturn<TValues extends Record<string, unknown>, K extends keyof TValues> = {
+  name: K;
+  value: TValues[K];
+  onChange: (value: TValues[K]) => void;
+  onBlur: () => void;
+};
+
+export type UseFormReturn<TValues extends Record<string, unknown>> = {
+  values: TValues;
+  errors: Errors<TValues>;
+  touched: Touched<TValues>;
+  isSubmitting: boolean;
+
+  register: <K extends keyof TValues>(name: K) => RegisterReturn<TValues, K>;
+
+  setValues: <K extends keyof TValues>(name: K, value: TValues[K]) => void;
+
+  validateField: <K extends keyof TValues>(name: K) => boolean;
+  validateAll: () => boolean;
+
+  handleSubmit: (onValid: (values: TValues) => void) => (e?: unknown) => void;
+
+  reset: (nextValues?: TValues) => void;
+};
